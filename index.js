198b320763e9b19901b40974b700e4b5c8453e00
rokib97 <hasanrokib1997@gmail.com>
2022-05-14 22:00:36 +0600

    added some api to fetch data to the client side

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.alyaj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client.db("doctors_portal").collection("booking");
+    const userCollection = client.db("doctors_portal").collection("users");
+
+    // get services data
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      //step 1: get the all services
+      const services = await serviceCollection.find().toArray();
+
+      //step 2: get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3 : for each service
+      services.map((service) => {
+        //step 4 : find bookings for that service
+        const serviceBooking = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        //step 5 : select slots for the service Booking
+        const bookedSlots = serviceBooking.map((book) => book.slot);
+        //step 6 : select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    /**
+     * API Naming Convention
+     * app.get('/booking') //get all booking in the collection or more than one by filter query
+     * app.get("/booking:id") // get a specific booking
+     * app.post("/booking:id")  // add a new booking
+     * app.patch("/booking:id")  // update a booking
+     * app.put('/booking:id) // upsert ==> update (if exists) or insert (if doesn't exists)
+     * app.delete("/booking:id")  // delete a booking
+     */
+
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello From Doctors Portal!");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors Portal app listening on port ${port}`);
+});
