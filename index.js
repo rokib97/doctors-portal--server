09ac99ae8e563b823a92975fa538c94eb1abf913
rokib97 <hasanrokib1997@gmail.com>
2022-05-17 02:47:01 +0600

    added api to add doctor info in the mongodb

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function run() {
       .collection("services");
     const bookingCollection = client.db("doctors_portal").collection("booking");
     const userCollection = client.db("doctors_portal").collection("users");
+    const doctorCollection = client.db("doctors_portal").collection("doctors");
 
     // get services data
     app.get("/service", async (req, res) => {
@@ -167,6 +168,13 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+    // doctor collection api
+    app.post("/doctor", async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+
+      res.send(result);
     });
   } finally {
     // await client.close();
